3156b2db9331672e98d0ab0c160b84a73204b275
Nani mo <115254370+Nani-mo@users.noreply.github.com>
2022-12-20 14:14:24 +0100

    Update how-many-names-with-A.py

--- how-many-names-with-A.py
+++ how-many-names-with-A.py
@@ -1,27 +1,27 @@
 print("It's a trap")
-import time
-import random
-from faker import Faker
+from faker import Faker #import the library faker for the random names
 fake = Faker()
 
 Counter=0
 
+#main function
 def operation():
 	counter = 0
-	cycles = int(input("Anzahl der Durchlaeufe (nur Ganzzahlen): "))
+	cycles = int(input("Anzahl der Durchlaeufe (nur Ganzzahlen): ")) #input of the grand letter and how many cycles the loop should be
 	letterToSearch = str(input("Anfanhsbuchstabe nach dem gesucht werden soll: "))
-	for n in range(cycles):
+	for n in range(cycles): #loop for [cycles] times
 		name = fake.name()
 		letter = (name[0])
 		if letter == letterToSearch:
 			counter = counter + 1
 			print (counter)
 	
-	counterDurchCycles = counter / cycles
+	counterDurchCycles = counter / cycles #calculate the percentage
 	Anteil = counterDurchCycles * 100
 	Anteil = float(Anteil)
-	print("\nvon " + str(cycles) + " random Namen begannen " + str(counter) + " mit einem '" + letterToSearch + "'")
+	print("\nvon " + str(cycles) + " random Namen begannen " + str(counter) + " mit einem '" + letterToSearch + "'") #output the result
 	print(str(round(Anteil, 2)) + "% der Gesammtmänge beginnen mit '" + letterToSearch + "'")
 
 operation()
 useless = str(input("\nEnter zum beenden"))
+
